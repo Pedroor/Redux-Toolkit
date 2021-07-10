8d86aca60839af689f16413aebb8120c8b559e68
Pedro Arthur <54337976+Pedroor@users.noreply.github.com>
2021-07-10 14:21:33 -0300

    feat: create Selector to optimize inline functions 🐱‍🏍

--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -1,8 +1,14 @@
+import { useCallback } from "react";
 import { useReduxSelector } from "../../hooks/useReduxSelector";
+import { selectCounterValue } from "../../store/slices/counter/selectors";
 import * as S from "./styles";
 
 const Counter = () => {
-  const counter = useReduxSelector(state => state.counter.value);
+  const counter = useReduxSelector(
+    useCallback(state => state.counter.value, [])
+  );
+
+  const counterTest = useReduxSelector(selectCounterValue);
 
   return <S.Container>{`${counter}`.padStart(2, "0")}</S.Container>;
 };
